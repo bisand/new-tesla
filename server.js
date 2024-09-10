b3248ceccc859f5c9f15afb3cf7805e592abfa3b
André Biseth <andre@biseth.net>
2024-09-10 08:23:47 +0000

    Reading version from file

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
 const teslaData = require('./tesla-data');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 // Set up Handlebars
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({
+    helpers: {
+        readFile: (filePath) => {
+            try {
+                const data = fs.readFileSync(filePath);
+                return data;
+            } catch (err) {
+                console.error('Error reading file:', err);
+                return '';
+            }
+        }
+    }
+}));
 app.set('view engine', 'handlebars');
 
 // Serve static files
 app.use(express.static('public'));
 
+// Helper function to read a file
+function readFile(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
 
 // Routes
 app.get('/', async (req, res) => {
